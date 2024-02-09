9c34185b5134ef8cf7b273eca8e80735c3a0531f
rubicon2 <liargent@hotmail.com>
2024-02-09 14:23:58 +0000

    Create basic hashmap class

---
+++ src/hashmap.js
@@ -0,0 +1,20 @@
+export default class Hashmap {
+  #capacity = 16;
+
+  #loadFactor = 0.75;
+
+  hash(key) {
+    // A deep, deep rabbit hole...
+    let hashCode = 0;
+
+    const prime = 31;
+    for (let i = 0; i < key.length; i += 1) {
+      hashCode = prime * hashCode + key.charCodeAt(i);
+      // As capacity expands, this will produce different results... ?
+      // I guess the values are shifted around as the capacity expands
+      hashCode %= this.#capacity;
+    }
+
+    return hashCode;
+  }
+}

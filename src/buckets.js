9d852d56c0204cab623db71e27dcfcc13ae2c196
rubicon2 <liargent@hotmail.com>
2024-02-09 15:03:55 +0000

    Implement buckets for hashmap
    Main purpose is to force usage of correct array
    indexes.

---
+++ src/buckets.js
@@ -0,0 +1,23 @@
+export default class Buckets {
+  #slots = [];
+
+  constructor(capacity) {
+    for (let i = 0; i < capacity; i += 1) {
+      this.#slots.push({});
+    }
+  }
+
+  getSlot(index) {
+    if (index < 0 || index >= this.#slots.length)
+      throw new Error('Trying to access index out of bounds');
+    return this.#slots[index];
+  }
+
+  get(index, key) {
+    return this.#slots[index][key];
+  }
+
+  set(index, key, value) {
+    this.#slots[index][key] = value;
+  }
+}
